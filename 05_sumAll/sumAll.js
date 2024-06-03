398e22ac1243cb84f848bb5bb026d2bbe641d0fe
ZakhaevK <zkupke@gmail.com>
2024-06-04 00:43:16 +1000

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  // My original error finder
+  // if (num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number" ) {
+  //   return "ERROR";
+  // }
+
+  // isInteger prevents negative values as well:
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  
+  const larger = (num1 > num2) ? num1 : num2;
+  const smaller = (larger == num1) ? num2 : num1;
+  let result = 0;
+ 
+  
+  for (let i = smaller; i <= larger; i++) {
+    result += i;
+    console.log(i);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
