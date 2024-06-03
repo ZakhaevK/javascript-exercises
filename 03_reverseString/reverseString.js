1dd0b30d1b51bf9e7e3780eb0730eaa758a67c95
ZakhaevK <zkupke@gmail.com>
2024-06-03 17:10:02 +1000

    Complete exercise 03_reverseString Implementation is not ideal, but commented better option from solution and it works regardless.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let result = '';
+  for (i = string.length; i >= 0; i--) {
+    result += string.charAt(i);
+  }
+  return result;
 };
+// This is quite arduous, the actual built in function way is:
+// return string.split("").reverse().join("");
+// This is also faster because of engine-level optimisation of built in functions.
 
 // Do not edit below this line
 module.exports = reverseString;
