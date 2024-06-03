0a0bf4b0a31b99abcc94bb7ab3684d94b87c673f
ZakhaevK <zkupke@gmail.com>
2024-06-03 17:41:29 +1000

    Complete exercise 04_removeFromArray Good solution implemented, may be better for memory efficiency, but filter solution is likely better

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+
+  for (let arg of values) {
+    // Work backwards because otherwise if numbers are in a row
+    // it will skip checking the same index which will be filled by
+    // the next number removed.
+    for (let num = array.length; num >= 0; num--) {
+      if (array[num] === arg) {
+        array.splice(num, 1);
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
