8b02c0020d3aced625a3e5fc2533adb583e33bff
ZakhaevK <zkupke@gmail.com>
2024-06-06 14:30:34 +1000

    Complete exercise 07_tempConversion Solution similar to provided, had type handling that may be unecessary

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  if (Number.isNaN(tempF)) {
+    return "ERROR"
+  }
+  return Math.round((tempF - 32) * (5/9) * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  if (Number.isNaN(tempC)) {
+    return "ERROR"
+  }
+return Math.round((tempC * (9/5) + 32) * 10) / 10;
 };
+
 
 // Do not edit below this line
 module.exports = {
