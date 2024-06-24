b0d608f8292fcfa7daef32ccdcf3fa6ec4fac534
ZakhaevK <zkupke@gmail.com>
2024-06-24 17:05:41 +1000

    Complete exercise 09_palindromes Solution is quite different but passes tests, might be less ideal not 100% sure

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let strArr = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "").split('');
+  console.log(strArr);
+  const isPalin = strArr.reduce((acc, char, i, arr) => {
+    return acc && (char == arr[arr.length - (1 + i)]);
+  }, true);
+  return isPalin;
 };
 
 // Do not edit below this line
