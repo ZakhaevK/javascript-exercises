057df82bc639a99acdbe0ef5d1ca22364310f416
ZakhaevK <zkupke@gmail.com>
2024-06-24 17:46:14 +1000

    Complete exercise 10_fibonacci Similar solution but minor differences

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+  num = Number.parseInt(num);
+  if (!Number.isInteger(num) || num < 0) return "OOPS"
+
+  let numArr = [];
+  for (let i = 0; num >= i; i++) {
+    if(numArr.length <= 1) {
+      numArr.push(i);
+    } else {
+      numArr.push((numArr[i - 2] + numArr[i - 1]));
+    }
+  }
+  console.log(numArr)
+  return numArr[num];
 };
 
 // Do not edit below this line
