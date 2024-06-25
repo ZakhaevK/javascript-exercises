3e3e1987ef76a6895548d6f0b3084198405ed049
ZakhaevK <zkupke@gmail.com>
2024-06-25 14:03:52 +1000

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+  if (!death) death = new Date().getFullYear();
+  return death - birth;
+} 
 
+const findTheOldest = function(peopleArr) {
+  return peopleArr.reduce((oldest, person) => {
+    personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+    oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    if (personAge > oldestAge) {
+      return person;
+    } else {
+      return oldest;
+    }
+  })
 };
 
 // Do not edit below this line
